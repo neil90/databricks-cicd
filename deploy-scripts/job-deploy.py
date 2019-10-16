f05b144d8b1c08fea4ca71c0c997355fd36d5ca2
viren.patel <neilpatel90@gmail.com>
2019-10-15 17:06:48 -0700

    Flake issues

--- deploy-scripts/job-deploy.py
+++ deploy-scripts/job-deploy.py
@@ -43,3 +43,5 @@ for job_json in job_jsons:
         print("Creating Job " + job_data["name"])
         api_client.post("/jobs/create", json_params=job_data)
 
+# Note to delete currently need to delete through
+# UI/API then remove json job_json folder
